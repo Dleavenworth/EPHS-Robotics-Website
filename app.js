7fe8b986667fdd83e15590730cf2f2a0caf49a0c
dleavenworth <dleavenworth.ep@mysummitps.org>
2016-10-07 16:26:59 +0000

    please

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.locals.basedir = path.join(__dirname, 'views');
 
 app.use('/', routes);
 app.use('/users', users);
